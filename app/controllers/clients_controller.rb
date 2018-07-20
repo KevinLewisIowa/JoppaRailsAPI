c6463f9b6e57551ba0a3484343f2a1b2ad7232fd
kevinlewis2 <kevinlewisiowa@gmail.com>
2018-07-20 20:33:52 +0000

    fixed it

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -54,7 +54,7 @@ class ClientsController < ApplicationController
   
   # GET /getClientRequestedItem?clientId={id}
   def getClientRequestedItem
-    @items = RequestedItem.where('client_id = ? AND has_received NOT IN (true)', params[:clientId])
+    @items = RequestedItem.where('client_id = ? AND (has_received IS NULL OR has_received = false', params[:clientId])
     
     render json: @items
   end
