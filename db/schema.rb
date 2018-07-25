8fc2a1dd8f64399cd1eb27e8b948a16b407dcd30
kevinlewis2 <kevinlewisiowa@gmail.com>
2018-07-25 13:38:29 +0000

    didn't think i added anything

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430185840) do
+ActiveRecord::Schema.define(version: 20180630163558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 20180430185840) do
     t.boolean  "is_completed"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "health_concerns", force: :cascade do |t|
+    t.integer  "client_id"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "location_camps", force: :cascade do |t|
