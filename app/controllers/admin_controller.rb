4003e993ebacf0e100d681fa84995cde8faf87ae
kevinlewis2 <kevinlewisiowa@gmail.com>
2018-08-20 20:50:38 +0000

    trying a thing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,9 @@ class AdminController < ApplicationController
     @routeMeals = ClientInteraction.joins(:client, {location_camp: {location: :route}})
                     .select("routes.name, sum(clients.number_meals) as totalNumberMeals")
                     .where("client_interactions.still_lives_here = ? 
-                      AND client_interactions.created_at > ?", true, DateTime.new(2017, 1, 1)).group("routes.name")
+                      AND client_interactions.created_at = ?", true,
+                      ClientInteraction.where("clients.id = :client.id AND location_camps.id = :location_camp_id")
+                        .max(:craeted_at)).group("routes.name")
     
     render json: @routeMeals
   end
