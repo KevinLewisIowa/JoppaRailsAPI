823b7a44a7cff5cb280f33fb37b8d70ab69bf70f
kevinlewis2 <kevinlewisiowa@gmail.com>
2018-08-21 16:32:51 +0000

    added subquery

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,9 +7,10 @@ class AdminController < ApplicationController
                         .joins('INNER JOIN locations as l on l.id = lc.location_id')
                         .joins('INNER JOIN routes as r on r.id = l.route_id')
                         .select('r.name, sum(c.number_meals) as totalNumberMeals')
-                        .where('client_interactions.still_lives_here = ? AND client_interactions.created_at = ?', true,
-                            ClientInteraction.where('client_id = ? AND location_camp_id = ?', 2, 3).maximum(:created_at))
+                        .where('client_interactions.still_lives_here = ? AND client_interactions.created_at = (SELECT MAX(created_at) from client_interactions where client_id = c.id)', true)
                         .group('r.name')
+    
+     #ClientInteraction.where('client_id = ? AND location_camp_id = ?', 2, 3).maximum(:created_at))
     
    # @routeMeals = ClientInteraction.joins('INNER JOIN location_camps as lc on lc.id = location_camp_id')
     #                    .joins('INNER JOIN clients as c on c.id = client_id')
