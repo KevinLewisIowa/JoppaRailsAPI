707ca2336da819dfe4bbec4a97d88a5e05dcf854
kevinlewis2 <kevinlewisiowa@gmail.com>
2018-09-27 19:43:53 +0000

    updated get current heaters for client

--- app/controllers/heaters_controller.rb
+++ app/controllers/heaters_controller.rb
@@ -68,7 +68,8 @@ class HeatersController < ApplicationController
   
   # GET /getCurrentHeatersForClient?clientId={id}
   def getCurrentHeatersForClient
-    @heaters = Heater.where("current_client_id = ? AND heater_status_id = ?", params[:clientId], 2)
+    
+    @heaters = Heaterjoins(:heater_type, :heater_status).select("heater_types.type_description, heaters.serial_number, heater_statuses.status_name, heaters.status_reason, heaters.is_active, heaters.updated_at").where("current_client_id = ? AND heater_status_id = ?", params[:clientId], 2)
     
     render json: @heaters
   end
