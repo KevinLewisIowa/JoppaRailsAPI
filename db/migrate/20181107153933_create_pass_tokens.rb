f8f9e7fa6b0b01e1ec982eaf0632d72e0001adec
kevinlewis2 <kevinlewisiowa@gmail.com>
2018-11-07 16:13:19 +0000

    fixed migration for token

--- db/migrate/20181107153933_create_pass_tokens.rb
+++ db/migrate/20181107153933_create_pass_tokens.rb
@@ -2,11 +2,8 @@ class CreatePassTokens < ActiveRecord::Migration[5.0]
   def change
     create_table :pass_tokens do |t|
       t.string :admin_password
-      t.string :string
       t.string :regular_password
-      t.string :string
       t.string :api_token
-      t.string :string
 
       t.timestamps
     end
