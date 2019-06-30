778286d01dba0a2ac8a5f231690a25498e62f36e
Tim Wilson <tim.wilson@joppa.org>
2019-06-30 22:11:34 +0000

    Add route for retrieving client pets

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get       'getClientPrayerRequests' => 'clients#getClientPrayerRequests'
   get       'getClientHealthConcerns' => 'health_concerns#getClientHealthConcerns'
   get       'getClientRequestedItem' => 'clients#getClientRequestedItem'
+  get       'getClientPets' => 'clients#getClientPets'
   get       'getClientsForCamp'  =>  'location_camps#getClientsForCamp'
   get       'getClientsByName'    => 'clients#getClientsByName'
   get       'getClientNotesForRoute'  => 'client_notes#getClientNotesForRoute'
