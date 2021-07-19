57ca6e000050e01e66ee35a17888abe2047d9117
Tim Wilson <tim.wilson@joppa.org>
2021-07-19 03:46:22 +0000

    Try to fix CORS error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins ['https://joppa-ui-test.herokuapp.com', 'https://joppa-ui-prod.herokuapp.com']
+     origins 'https://joppa-ui-prod.herokuapp.com'
 
      resource '*',
        headers: :any,
