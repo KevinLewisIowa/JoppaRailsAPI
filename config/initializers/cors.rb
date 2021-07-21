906aded6097347592eea20b9f536590c5c03a997
Tim Wilson <tim.wilson@joppa.org>
2021-07-21 03:15:29 +0000

    Update cors file to allow all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://joppa-ui-prod.herokuapp.com'
+     origins '*'
 
      resource '*',
        headers: :any,
