b1677cac86d31d66307fd036777439a1112b7f9a
Tim Wilson <tim.wilson@joppa.org>
2021-07-21 03:20:47 +0000

    Remove CORS from application.rb

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module Workspace
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     # Access-Control-Allow-Origin
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:4200', /https*:\/\/.*?herokuapp\.com/
-        resource '*', :headers => :any, :methods => :any
-      end
-    end
+    #config.middleware.insert_before 0, Rack::Cors do
+    #  allow do
+    #    origins 'localhost:4200', /https*:\/\/.*?herokuapp\.com/
+    #    resource '*', :headers => :any, :methods => :any
+    #  end
+    #end
   end
 end
