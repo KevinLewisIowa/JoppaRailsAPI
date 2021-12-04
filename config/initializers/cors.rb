ee26010eed1a37ccf9f079f986726fd6a1246318
Joppa Development <development@joppa.org>
2021-12-04 23:31:28 +0000

    modify cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-     origins 'https://joppa-ui-test.herokuapp.com, https://joppa-ui-prod.herokuapp.com'
+     origins 'herokuapp.com'
      resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
