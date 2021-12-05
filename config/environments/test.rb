72e74e3ca389a28d456f164bf8a896470201f917
Joppa Development <development@joppa.org>
2021-12-05 15:12:39 +0000

    Update hosts in environment files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
   
-  config.hosts << "joppa-ui-prod.herokuapp.com"
+  config.hosts << "*.herokuapp.com"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
