a79fad0934e6d7d5a722be84ccff1863551476ff
Joppa Development <development@joppa.org>
2021-12-09 14:50:37 +0000

    Add default client_tents routes (CRUD)

--- config/routes.rb
+++ config/routes.rb
@@ -92,5 +92,6 @@ Rails.application.routes.draw do
     resources :routes
     resources :users
     resources :route_instances
+    resources :client_tents
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
