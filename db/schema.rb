c9ddbf3505eadc9691f4848bbaa1f1c9b4d17965
Tim Wilson <tim.wilson@joppa.org>
2023-03-08 01:48:09 +0000

    Upgrade schema with new column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_30_194728) do
+ActiveRecord::Schema.define(version: 2023_03_08_014509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 2022_12_30_194728) do
     t.integer "client_id"
     t.string "pet_name"
     t.integer "age"
+    t.string "breed"
   end
 
   create_table "client_referrals", force: :cascade do |t|
