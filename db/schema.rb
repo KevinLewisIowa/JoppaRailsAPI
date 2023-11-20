e421e7f94685687cdd67660974305ad032eb8c20
Tim Wilson <tim.wilson@joppa.org>
2023-11-20 04:25:41 +0000

    Add latitude and longitude to client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_24_195931) do
+ActiveRecord::Schema.define(version: 2023_11_20_041926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -180,6 +180,8 @@ ActiveRecord::Schema.define(version: 2023_06_24_195931) do
     t.binary "client_picture"
     t.string "race"
     t.string "ethnicity"
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.index ["first_name", "last_name"], name: "index_clients_on_first_name_and_last_name", unique: true
   end
 
