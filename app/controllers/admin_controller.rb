7028976c88852109a120e044fa05a1e807ed6908
Tim Wilson <tim.wilson@joppa.org>
2024-02-26 20:44:52 +0000

    Make admin pulls memory efficient

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   
   # GET /getAdminSumNumberMeals 
   def getAdminRouteNumberMeals
+    route_meals_list = [];
     @routeMeals = ClientInteraction.joins('INNER JOIN location_camps as lc on lc.id = location_camp_id')
                         .joins('INNER JOIN clients as c on c.id = client_id')
                         .joins('INNER JOIN routes as r on r.id = lc.route_id')
@@ -11,42 +12,48 @@ class AdminController < ApplicationController
                             (SELECT MAX(created_at) from client_interactions 
                             where client_id = c.id and location_camp_id = lc.id) AND 
                             r.is_active = ? AND lc.is_active = ?', true, true, true)
-                        .group('r.name')
-    
-     #ClientInteraction.where('client_id = ? AND location_camp_id = ?', 2, 3).maximum(:created_at))
-    
-   # @routeMeals = ClientInteraction.joins('INNER JOIN location_camps as lc on lc.id = location_camp_id')
-    #                    .joins('INNER JOIN clients as c on c.id = client_id')
-     #                   .joins('INNER JOIN routes as r on r.id = lc.route_id')
-       #                 .select('r.name, sum(c.number_meals) as totalNumberMeals')
-        #                .where('still_lives_here = ?', true)
-         #               .group('r.name')
+                        .group('r.name').find_each do |meal|
+      route_meals_list.push(meal)
+    end
+    @routeMeals = route_meals_list;
     
     render json: @routeMeals 
   end
   
   # GET /getAdminRouteUndeliveredItems 
   def getAdminRouteUndeliveredItems
-    @routeUndeliveredItems = Client.joins('INNER JOIN requested_items AS ri ON clients.id = ri.client_id')
-        .joins('INNER JOIN location_camps AS lc ON clients.current_camp_id = lc.id')
-        .joins('INNER JOIN routes AS r ON lc.route_id = r.id')
-        .select("r.name, clients.first_name, clients.preferred_name, clients.last_name, ri.id, ri.client_id, ri.item_description, ri.date_requested, ri.fulfilled, ri.created_at").where("ri.has_received = ?", false)
-        .group("r.name, clients.first_name, clients.preferred_name, clients.last_name, ri.id, ri.client_id, ri.item_description, ri.date_requested, ri.created_at, ri.fulfilled")
-        .order("r.name, clients.last_name, clients.first_name")
+    undelivered_items_list = [];
+    Client.joins('INNER JOIN requested_items AS ri ON clients.id = ri.client_id')
+    .joins('INNER JOIN location_camps AS lc ON clients.current_camp_id = lc.id')
+    .joins('INNER JOIN routes AS r ON lc.route_id = r.id')
+    .select("r.name, clients.first_name, clients.preferred_name, clients.last_name, ri.id, ri.client_id, ri.item_description, ri.date_requested, ri.fulfilled, ri.created_at").where("ri.has_received = ?", false)
+    .group("r.name, clients.first_name, clients.preferred_name, clients.last_name, ri.id, ri.client_id, ri.item_description, ri.date_requested, ri.created_at, ri.fulfilled")
+    .order("r.name, clients.last_name, clients.first_name").find_each do |item|
+      undelivered_items_list.push(item)
+    end
+    @routeUndeliveredItems = undelivered_items_list
     
     render json: @routeUndeliveredItems
   end
   
   # GET /getAdminRouteUnfulfilledGoalsNextSteps
   def getAdminRouteUnfulfilledGoalsNextSteps
-    @routeUnfulfilledGoalsNextSteps = ClientInteraction.joins({client: :goals_and_next_steps}, {location_camp: :route}).select("routes.name, clients.first_name, clients.preferred_name, clients.last_name, goals_and_next_steps.description").where("goals_and_next_steps.is_completed = ?", false).group("routes.name, clients.first_name, clients.preferred_name, clients.last_name, goals_and_next_steps.description")
+    unfulfilled_goals = [];
+    ClientInteraction.joins({client: :goals_and_next_steps}, {location_camp: :route}).select("routes.name, clients.first_name, clients.preferred_name, clients.last_name, goals_and_next_steps.description").where("goals_and_next_steps.is_completed = ?", false).group("routes.name, clients.first_name, clients.preferred_name, clients.last_name, goals_and_next_steps.description").find_each do |goal|
+      unfulfilled_goals.push(goal)
+    end
+    @routeUnfulfilledGoalsNextSteps = unfulfilled_goals
     
     render json: @routeUnfulfilledGoalsNextSteps
   end
   
   # GET /getAdminRouteUnfulfilledPrayerRequestsNeeds
   def getAdminRouteUnfulfilledPrayerRequestsNeeds
-    @routeUnfulfilledPrayerRequestsNeeds = ClientInteraction.joins({client: :prayer_request_and_needs}, {location_camp: :route}).select("routes.name, clients.first_name, clients.preferred_name, clients.last_name, prayer_request_and_needs.detail").where("prayer_request_and_needs.is_completed = ?", false).group("routes.name, clients.first_name, clients.preferred_name, clients.last_name, prayer_request_and_needs.detail")
+    unfulfilled_needs = [];
+    ClientInteraction.joins({client: :prayer_request_and_needs}, {location_camp: :route}).select("routes.name, clients.first_name, clients.preferred_name, clients.last_name, prayer_request_and_needs.detail").where("prayer_request_and_needs.is_completed = ?", false).group("routes.name, clients.first_name, clients.preferred_name, clients.last_name, prayer_request_and_needs.detail").find_each do |prayer|
+      unfulfilled_needs.push(prayer)
+    end
+    @routeUnfulfilledPrayerRequestsNeeds = unfulfilled_needs;
     
     render json: @routeUnfulfilledPrayerRequestsNeeds
   end
@@ -60,7 +67,12 @@ class AdminController < ApplicationController
       @lostHeaters = Heater.where(heater_status_id: 5).count
       @destroyedHeaters = Heater.where(heater_status_id: 6).count
       @stolenHeaters = Heater.where(heater_status_id: 4).count
-      @heaterList = Heater.joins('INNER JOIN clients as cl on cl.id = current_client_id').where(heater_status_id:2).select('heaters.serial_number, cl.first_name, cl.preferred_name, cl.last_name')
+      
+      heater_list = [];
+      Heater.joins('INNER JOIN clients as cl on cl.id = current_client_id').where(heater_status_id:2).select('heaters.serial_number, cl.first_name, cl.preferred_name, cl.last_name').find_each do |heater|
+        heater_list.push(heater)
+      end
+      @heaterList = heater_list;
       
       render json: { tanksOut: @tanksOut, hosesOut: @hosesOut, heatersOut: @heatersOut, brokenHeaters: @brokenHeaters, lostHeaters: @lostHeaters, destroyedHeaters: @destroyedHeaters, stolenHeaters: @stolenHeaters, heaterList: @heaterList}
   end
@@ -78,7 +90,11 @@ class AdminController < ApplicationController
   
   # GET /getFirstTimeHomelessnessReport
   def getFirstTimeHomelessnessReport
-      @homelessnessInfo = Client.select('first_name, preferred_name, last_name, birth_date, phone, first_time_homeless, date_became_homeless, homeless_reason')
+      first_time_homeless = [];
+      Client.select('first_name, preferred_name, last_name, birth_date, phone, first_time_homeless, date_became_homeless, homeless_reason').find_each do |client|
+        first_time_homeless.push(client)
+      end
+      @homelessnessInfo = first_time_homeless
       
       render json: @homelessnessInfo
   end
