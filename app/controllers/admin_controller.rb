17af5561625fd9b824102349bdbf4936190b6440
Tim Wilson <tim.wilson@joppa.org>
2024-02-27 04:21:03 +0000

    Make meals endpoint correct

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,7 @@ class AdminController < ApplicationController
     .joins('JOIN routes as r on r.id = lc.route_id')
     .select('r.name, sum(c.number_meals) as totalNumberMeals')
     .where('client_interactions.still_lives_here = ? AND 
-        c.current_camp_id IS NOT NULL AND 
+        c.current_camp_id IS NOT NULL AND c.current_camp_id > 0 AND c.current_camp_id <> 449 AND
         r.is_active = ? AND lc.is_active = ?', true, true, true)
     .group('r.name')
     
