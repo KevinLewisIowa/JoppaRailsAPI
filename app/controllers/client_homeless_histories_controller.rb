80285e4ea9f027d375ecaded889ec5a3cde6a776
Tim Wilson <wilsont3@central.edu>
2026-07-18 14:52:10 -0500

    Add fromDate, toDate, and clientId parameters to the client_homeless_histories table

--- app/controllers/client_homeless_histories_controller.rb
+++ app/controllers/client_homeless_histories_controller.rb
@@ -3,7 +3,7 @@ class ClientHomelessHistoriesController < ApplicationController
 
   # GET /client_homeless_histories
   def index
-    @client_homeless_histories = ClientHomelessHistory.all
+    @client_homeless_histories = filtered_client_homeless_histories
 
     render json: @client_homeless_histories
   end
@@ -59,6 +59,35 @@ class ClientHomelessHistoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_client_homeless_history
       @client_homeless_history = ClientHomelessHistory.find(params[:id])
+    end
+
+    def filtered_client_homeless_histories
+      relation = ClientHomelessHistory.all
+
+      start_date = parse_date_param(params[:fromDate] || params[:from_date])
+      end_date = parse_date_param(params[:toDate] || params[:to_date])
+
+      if start_date && end_date && start_date > end_date
+        return relation.none
+      end
+
+      relation = relation.where('date_became_homeless >= ?', start_date) if start_date
+      relation = relation.where('date_became_homeless <= ?', end_date) if end_date
+
+      client_id_filter = params[:clientId] || params[:client_id]
+      unless client_id_filter.blank? || client_id_filter.to_s.casecmp('ALLCLIENTS').zero?
+        relation = relation.where(client_id: client_id_filter)
+      end
+
+      relation.order(date_became_homeless: :desc, created_at: :desc)
+    end
+
+    def parse_date_param(value)
+      return if value.blank?
+
+      Date.parse(value.to_s)
+    rescue ArgumentError
+      nil
     end
 
     # Only allow a list of trusted parameters through.
--- test/controllers/client_homeless_histories_controller_test.rb
+++ test/controllers/client_homeless_histories_controller_test.rb
@@ -10,6 +10,30 @@ class ClientHomelessHistoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should filter index by fromDate/toDate and client_id" do
+    in_range_history = ClientHomelessHistory.create!(client_id: 1, first_time_homeless: true, date_became_homeless: Date.parse('2024-01-15'), note: 'in range')
+    ClientHomelessHistory.create!(client_id: 2, first_time_homeless: true, date_became_homeless: Date.parse('2024-01-20'), note: 'other client')
+    ClientHomelessHistory.create!(client_id: 1, first_time_homeless: true, date_became_homeless: Date.parse('2024-02-01'), note: 'outside range')
+
+    get client_homeless_histories_url, params: { fromDate: '2024-01-01', toDate: '2024-01-31', client_id: '1' }, as: :json
+
+    assert_response :success
+    response_ids = JSON.parse(response.body).map { |history| history['id'] }
+    assert_equal [in_range_history.id], response_ids
+  end
+
+  test "should return all clients when clientId is ALLCLIENTS" do
+    in_range_history = ClientHomelessHistory.create!(client_id: 1, first_time_homeless: true, date_became_homeless: Date.parse('2024-01-15'), note: 'first client')
+    other_client_history = ClientHomelessHistory.create!(client_id: 2, first_time_homeless: true, date_became_homeless: Date.parse('2024-01-20'), note: 'second client')
+
+    get client_homeless_histories_url, params: { fromDate: '2024-01-01', toDate: '2024-01-31', clientId: 'ALLCLIENTS' }, as: :json
+
+    assert_response :success
+    response_ids = JSON.parse(response.body).map { |history| history['id'] }
+    assert_includes response_ids, in_range_history.id
+    assert_includes response_ids, other_client_history.id
+  end
+
   test "should create client_homeless_history" do
     assert_difference('ClientHomelessHistory.count') do
       post client_homeless_histories_url, params: { client_homeless_history: { client_id: @client_homeless_history.client_id, date_became_homeless: @client_homeless_history.date_became_homeless, first_time_homeless: @client_homeless_history.first_time_homeless, note: @client_homeless_history.note, reason_for_homelessness: @client_homeless_history.reason_for_homelessness } }, as: :json
