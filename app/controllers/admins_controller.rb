e51f35126da6135b5ed5dd983d7759cc3904656c
Tim Wilson <tim.wilson42850@gmail.com>
2026-09-02 22:32:13 -0500

    Remove skip_before_action from admins_controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,4 @@
 class AdminsController < ApplicationController
-  skip_before_action :verify_authenticity_token
   before_action :authenticate_admin!
   before_action :authorize_super_admin!, except: [:show, :update_own_profile]
   before_action :set_admin, only: [:show, :update, :destroy, :reset_password]
